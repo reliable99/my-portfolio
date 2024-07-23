1920407fbef0341dea9a66b53ea9804b823e3bdc
relaible99 <abdulmuhminsodiq@gmail.com>
2024-07-23 13:41:39 +0100

    first

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Abdulmuhmin, a Sofware Developer based  in Nigeria.
+            Hi! I&apos;m Abdulmuhmin, a Frontend Developer based  in Nigeria.
           </p>
 
           <a href="#about">
